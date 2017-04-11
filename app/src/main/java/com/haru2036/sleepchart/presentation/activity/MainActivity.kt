1ec4f9808ae43a74d5f5962a26c04ce2b46c73a6
haru2036 <haru2036@gmail.com>
2017-04-11 18:41:12 +0900

    add fetch sleeps function

--- app/src/main/java/com/haru2036/sleepchart/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/haru2036/sleepchart/presentation/activity/MainActivity.kt
@@ -2,12 +2,17 @@ package com.haru2036.sleepchart.presentation.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import android.widget.TextView
 import com.haru2036.sleepchart.R
 import com.haru2036.sleepchart.app.SleepChart
 import com.haru2036.sleepchart.di.module.SleepModule
 import com.haru2036.sleepchart.domain.usecase.SleepUseCase
+import com.haru2036.sleepchart.presentation.adapter.MainSleepAdapter
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -19,15 +24,32 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var sleepUsecase: SleepUseCase
 
+    val subscriptions = CompositeSubscription()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listView = findViewById(R.id.activity_main_list) as ListView
         SleepChart.getAppComponent().plus(SleepModule(SleepChart.getRetrofit())).inject(this)
-        sleepUsecase.fetchSleeps()
+        subscriptions.add(sleepUsecase.fetchSleeps()
                 .delay(500, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ Timber.d(it.toString(), it.toString())},{ Timber.e(it.toString(), it.toString())})
+                .subscribe({
+                    Log.d(it.toString(), it.toString())
+                    listView.adapter = MainSleepAdapter(this, it)
+                },{
+                    Log.e(it.toString(), it.toString())
+                })
+        )
+    }
 
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        subscriptions.unsubscribe()
     }
 }
---
+++ app/src/main/java/com/haru2036/sleepchart/presentation/adapter/MainSleepAdapter.kt
@@ -0,0 +1,25 @@
+package com.haru2036.sleepchart.presentation.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.haru2036.sleepchart.databinding.ItemSleepsBinding
+import com.haru2036.sleepchart.domain.entity.Sleep
+
+/**
+ * Created by haru2036 on 2017/03/29.
+ */
+
+class MainSleepAdapter(context: Context, items:List<Sleep>) : ArrayAdapter<Sleep>(context, 0, items) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val binding = ItemSleepsBinding.inflate(layoutInflater, parent, false)
+        binding.sleep = getItem(position)
+        return binding.root
+    }
+    
+
+}
