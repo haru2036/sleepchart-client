e6ead7df10b63d62c923d10164215e73c7e1bfc3
haru2036 <haru2036@gmail.com>
2019-02-02 18:21:21 +0900

    デフォルト値が抜けてたのを修正

--- app/src/main/java/com/haru2036/sleepchart/infra/SharedPreferencesAccessor.kt
+++ app/src/main/java/com/haru2036/sleepchart/infra/SharedPreferencesAccessor.kt
@@ -2,12 +2,13 @@ package com.haru2036.sleepchart.infra
 
 import android.content.Context
 import android.preference.PreferenceManager
+import com.haru2036.sleepchart.R
 import io.reactivex.Single
 import javax.inject.Inject
 
 class SharedPreferencesAccessor @Inject constructor(val context: Context){
     private val sharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(context)}
 
-    fun getGadgetBridgePath() = Single.create<String> { it.onSuccess(sharedPreferences.getString("pref_key_gadgetbridge_import_path", "")) }
+    fun getGadgetBridgePath() = Single.create<String> { it.onSuccess(sharedPreferences.getString("pref_key_gadgetbridge_import_path", context.getString(R.string.settings_default_db_path))) }!!
 }
 
