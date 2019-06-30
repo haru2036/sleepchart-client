c73ca124c2b70c87a435ce85063db48b34e5e7eb
haru2036 <haru2036@gmail.com>
2019-06-30 12:21:29 +0900

    tmp: google sign in をつけてみた(ビルド通らない)

---
+++ app/src/main/java/com/haru2036/sleepchart/LoginActivity.kt
@@ -0,0 +1,56 @@
+package com.haru2036.sleepchart
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.GoogleApiClient
+
+class LoginActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListener {
+    private val RC_SIGN_IN = 1
+
+    private val signInButton by lazy {
+        findViewById<SignInButton>(R.id.sign_in_button)
+    }
+
+    private val statusText by lazy{
+        findViewById<TextView>(R.id.sign_in_status_text)
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        val googleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+        statusText.text = "未認証"
+        signInButton.setOnClickListener {
+            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(resultCode){
+            RC_SIGN_IN -> {
+                val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+                statusText.text = result.toString()
+            }
+
+        }
+    }
+}
