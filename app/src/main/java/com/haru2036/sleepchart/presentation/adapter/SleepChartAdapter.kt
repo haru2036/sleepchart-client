3f59d56c03478df61aeb56d4974e1370e9e03f20
haru2036 <haru2036@gmail.com>
2019-07-25 20:51:22 +0900

    androidx対応

--- app/src/main/java/com/haru2036/sleepchart/presentation/adapter/SleepChartAdapter.kt
+++ app/src/main/java/com/haru2036/sleepchart/presentation/adapter/SleepChartAdapter.kt
@@ -1,10 +1,10 @@
 package com.haru2036.sleepchart.presentation.adapter
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.haru2036.sleepchart.R
 import com.haru2036.sleepchart.domain.entity.Sleep
 import com.haru2036.sleepchart.presentation.TimeChartView
@@ -14,6 +14,29 @@ import java.util.*
 
 
 class SleepChartAdapter(val context: Context) : RecyclerView.Adapter<SleepChartAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(inflater.inflate(R.layout.item_sleepchart_day, parent, false) as FrameLayout)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val view = holder?.itemView as FrameLayout
+        val timeChartView = view.findViewById<TimeChartView>(R.id.view_time_chart)
+        timeChartView.clearAllSleeps()
+        timeChartView.sleeps = sleepsOfDays.toList()[position].second
+        Calendar.getInstance().apply {
+            time = timeChartView.sleeps.first().end
+
+            val backgroundColor = context.getColor(
+                    when (get(Calendar.DAY_OF_WEEK)) {
+                        Calendar.SUNDAY -> R.color.sundayBackground
+                        Calendar.SATURDAY -> R.color.saturdayBackground
+                        else -> R.color.normalBackground
+                    }
+            )
+            timeChartView.setBackgroundColor(backgroundColor)
+            view.invalidate()
+        }
+    }
+
     private val inflater: LayoutInflater by lazy { LayoutInflater.from(context) }
     private val nightOffset = 4L //前日の20~23時を当日扱いにするためのオフセット
     var items: List<Sleep> = emptyList()
@@ -35,30 +58,6 @@ class SleepChartAdapter(val context: Context) : RecyclerView.Adapter<SleepChartA
     private var sleepsOfDays: MutableMap<Int, MutableList<Sleep>> = mutableMapOf()
 
     override fun getItemCount(): Int = sleepsOfDays.size
-
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder =
-            ViewHolder(inflater.inflate(R.layout.item_sleepchart_day, parent, false) as FrameLayout)
-
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        val view = holder?.itemView as FrameLayout
-        val timeChartView = view.findViewById<TimeChartView>(R.id.view_time_chart)
-        timeChartView.clearAllSleeps()
-        timeChartView.sleeps = sleepsOfDays.toList()[position].second
-        Calendar.getInstance().apply {
-            time = timeChartView.sleeps.first().end
-
-            val backgroundColor = context.getColor(
-                when (get(Calendar.DAY_OF_WEEK)){
-                    Calendar.SUNDAY -> R.color.sundayBackground
-                    Calendar.SATURDAY -> R.color.saturdayBackground
-                    else -> R.color.normalBackground
-                }
-            )
-            timeChartView.setBackgroundColor(backgroundColor)
-            view.invalidate()
-        }
-    }
-
 
     class ViewHolder constructor(row: FrameLayout) : RecyclerView.ViewHolder(row) {
 
