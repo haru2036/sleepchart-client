50b798180eac35779eef4c9c65280d2659a51549
haru2036 <haru2036@gmail.com>
2019-11-25 14:26:46 +0900

    コミット漏れを追加

---
+++ app/src/main/java/com/haru2036/sleepchart/presentation/viewmodel/SleepDetailViewModel.kt
@@ -0,0 +1,38 @@
+package com.haru2036.sleepchart.presentation.viewmodel
+
+import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
+import com.haru2036.sleepchart.domain.entity.Sleep
+import com.haru2036.sleepchart.domain.usecase.SleepUseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.inject.Inject
+
+class SleepDetailViewModel @Inject constructor(private val usecase: SleepUseCase) : ViewModel() {
+    var sleep: Sleep? = null
+
+    var formattedSleepStart = ObservableField<String>()
+    var formattedSleepEnd = ObservableField<String>()
+    var sleepRating = BehaviorSubject.create<Int>()
+
+    fun loadSleep(id: Long) {
+        usecase.getSleepById(id)
+                .singleOrError()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    //todo: フォーマットは暫定対応
+                    sleep = it
+                    formattedSleepStart.set(SimpleDateFormat("MM/dd\nHH:mm", Locale.getDefault()).format(it.start))
+                    formattedSleepEnd.set(SimpleDateFormat("MM/dd\nHH:mm", Locale.getDefault()).format(it.end))
+                    it.rating?.let { sleepRating.onNext(it) }
+                },{
+                    Timber.e(it)
+                })
+
+    }
+}
